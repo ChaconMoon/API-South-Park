b32ac07af485a106c353ef1c0034e9352c64ab25
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-09-02 11:34:58 +0200

    Refactor special response

--- src/controller/specials_controller.py
+++ src/controller/specials_controller.py
@@ -14,7 +14,7 @@ def get_special_by_id(id: int, add_url=False, base_url=""):
         )
 
         for row in query_result:
-            episode_info = Special(
+            special_info = Special(
                 id=int(row[0]) if row[0] is not None else 0,
                 title=str(row[1]) if row[1] is not None else "",
                 release_date=str(row[2]) if row[2] is not None else "",
@@ -22,13 +22,16 @@ def get_special_by_id(id: int, add_url=False, base_url=""):
                 link=str(row[4]) if row[4] is not None else "",
             )
         if add_url:
-            result = episode_info.model_dump()
+            result = dict()
+            result["name"] = special_info.model_dump()["name"]
             result["url"] = f"{base_url}special/{row[0]}"
             return result
-        return episode_info.model_dump()
-
-        # Si no se encontró el personaje
-        # return {"error": "Character not found", "status": "not_found"}
+        query_result = get_query_result(text("SELECT * FROM public.specials"))
+        result = dict()
+        result["special"] = special_info.model_dump()
+        result["metadata"] = dict()
+        result["metadata"]["total_specials_in_database"] = query_result.rowcount
+        return result
 
     except Exception as e:
         return {"error": str(e), "status": "failed"}
