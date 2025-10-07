03d8ae985637a75c5f91b0323daefeafff497c7a
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-10-07 12:00:23 +0200

    Finish family section in website

--- src/website_elements/create_families_card_web.py
+++ src/website_elements/create_families_card_web.py
@@ -13,17 +13,27 @@ def create_family_web_card_to_website(base_url: str, family_id: int) -> str:
     family_images = request_json["family"]["images"]
     family_members = request_json["family"]["members"]
 
+    members_div = ""
+
+    for family_member in family_members:
+        members_div += f"""<div class="family_member">
+        <p> - {family_member["name"]}</p>
+        </div>\n"""
+
     html_response = f"""
         <a href="{request_url}" class="family_card_link">
-        <div class="family_card">
-        <img class="family_image" src="{family_images[0]}" alt="Avatar">
-        <p class="family_number">
-        <b>ID: {family_id}</b>
-        </p>
-        <p class="family_name">
-        <b>{family_name}</b> 
-        </p>
-        </div>
+                <div class="family_card">
+                        <img class="family_image" src="{family_images[0]}" alt="Avatar"/>
+                        <p class="family_name">
+                                <b>{family_name}</b> 
+                        </p>
+                                <p class="family_number">
+                                        <b>ID: {family_id}</b>
+                                </p>
+                        <div class="family_members_container">
+                        {members_div}
+                        </div>
+                </div>
         </a>\n
         """
     return html_response
